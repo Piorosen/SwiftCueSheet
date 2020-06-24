b5f6e6b3f0f97b646a1929c086e5e04b82e0bfa3
Piorosen <aoikazto@gmail.com>
2020-06-24 13:22:48 +0900

    Resource to static let

---
+++ Tests/CueSheetTests/Resources/Resources.swift
@@ -0,0 +1,128 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aoikazto on 2020/06/24.
+//
+
+import Foundation
+
+struct Resources {
+    static let MYTH_and_ROID_cue = """
+REM GENRE anime
+REM DATE 2018
+REM DISCID 3B03B104
+REM COMMENT "ExactAudioCopy v1.3"
+CATALOG 4935228173068
+PERFORMER "MYTH&ROID"
+TITLE "HYDRA"
+REM COMPOSER ""
+FILE "MYTH&ROID - HYDRA.wav" WAVE
+  TRACK 01 AUDIO
+    TITLE "HYDRA"
+    PERFORMER "MYTH&ROID"
+    REM COMPOSER ""
+    ISRC JPD731700610
+    INDEX 01 00:00:00
+  TRACK 02 AUDIO
+    TITLE "Stormy Glory"
+    PERFORMER "MYTH&ROID"
+    REM COMPOSER ""
+    ISRC JPD731700611
+    INDEX 00 04:51:49
+    INDEX 01 04:54:19
+  TRACK 03 AUDIO
+    TITLE "HYDRA (instrumental)"
+    PERFORMER "MYTH&ROID"
+    REM COMPOSER ""
+    ISRC JPD731700612
+    INDEX 00 07:50:61
+    INDEX 01 07:55:31
+  TRACK 04 AUDIO
+    TITLE "Stormy Glory (instrumental)"
+    PERFORMER "MYTH&ROID"
+    REM COMPOSER ""
+    ISRC JPD731700613
+    INDEX 00 12:44:64
+    INDEX 01 12:47:34
+"""
+    
+    static let Faithless_Live_in_Berlin_cue = """
+REM GENRE Electronica
+REM DATE 1998
+PERFORMER "Faithless"
+TITLE "Live in Berlin"
+FILE "Faithless - Live in Berlin.mp3" MP3
+  TRACK 01 AUDIO
+    TITLE "Reverence"
+    PERFORMER "Faithless"
+    INDEX 01 00:00:00
+  TRACK 02 AUDIO
+    TITLE "She's My Baby"
+    PERFORMER "Faithless"
+    INDEX 01 06:42:00
+  TRACK 03 AUDIO
+    TITLE "Take the Long Way Home"
+    PERFORMER "Faithless"
+    INDEX 01 10:54:00
+  TRACK 04 AUDIO
+    TITLE "Insomnia"
+    PERFORMER "Faithless"
+    INDEX 01 17:04:00
+  TRACK 05 AUDIO
+    TITLE "Bring the Family Back"
+    PERFORMER "Faithless"
+    INDEX 01 25:44:00
+  TRACK 06 AUDIO
+    TITLE "Salva Mea"
+    PERFORMER "Faithless"
+    INDEX 01 30:50:00
+  TRACK 07 AUDIO
+    TITLE "Dirty Old Man"
+    PERFORMER "Faithless"
+    INDEX 01 38:24:00
+  TRACK 08 AUDIO
+    TITLE "God Is a DJ"
+    PERFORMER "Faithless"
+    INDEX 01 42:35:00
+
+"""
+
+    static let Question_cue = 
+    """
+    REM GENRE Anime
+    REM DATE 2016
+    REM DISCID 21038104
+    REM COMMENT "ExactAudioCopy v1.0b3"
+    CATALOG 4562475258103
+    PERFORMER "3年E組うた担(渚&茅野&業&磯貝&前原)"
+    TITLE "QUESTION"
+    FILE "3年E組うた担(渚&茅野&業&磯貝&前原)  - QUESTION.wav" WAVE
+      TRACK 01 AUDIO
+        TITLE "QUESTION"
+        PERFORMER "3年E組うた担(渚&茅野&業&磯貝&前原)"
+        ISRC JPT551600062
+        INDEX 01 00:00:00
+      TRACK 02 AUDIO
+        TITLE "3年E組うた担"
+        PERFORMER "3年E組うた担(渚&茅野&業&磯貝&前原)"
+        ISRC JPT551600090
+        INDEX 00 04:26:17
+        INDEX 01 04:29:27
+      TRACK 03 AUDIO
+        TITLE "QUESTION (Instrumental)"
+        PERFORMER "3年E組うた担(渚&茅野&業&磯貝&前原)"
+        ISRC JPT551600092
+        INDEX 00 07:26:71
+        INDEX 01 07:30:07
+      TRACK 04 AUDIO
+        TITLE "3年E組うた担 (Instrumental)"
+        PERFORMER "3年E組うた担(渚&茅野&業&磯貝&前原)"
+        ISRC JPT551600091
+        INDEX 00 11:56:12
+        INDEX 01 11:59:22
+
+
+"""
+    
+}
