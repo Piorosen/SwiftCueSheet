acb4756bdfc66475b3c70c87d192089cff5b96e4
Piorosen <aoikazto@gmail.com>
2020-10-21 14:58:11 +0900

    refactor : 코드 중복 삭제

--- Sources/SwiftCueSheet/CueSheet.swift
+++ Sources/SwiftCueSheet/CueSheet.swift
@@ -38,6 +38,7 @@ public struct CueSheet {
     #endif
     
     
+    // MARK: - 시간 데이터 계산 코드 중복 삭제 하기 위함
     #if canImport(AVFoundation)
     public func calcTime(lengthOfMusic: Double? = nil) -> [CSLengthOfAudio] {
         var length:Double = 0
@@ -47,41 +48,15 @@ public struct CueSheet {
             length = ownAudioLength
         }
         
-        var calcStartTime = 0.0
-        var result = [CSLengthOfAudio]()
-        
-        for index in self.file.tracks.indices {
-            var dur = 0.0
-            let lastIndex = file.tracks[index].index.count - 1
-            
-            if index != file.tracks.count - 1 {
-                let me = file.tracks[index].index[lastIndex].indexTime.frames
-                let next = file.tracks[index + 1].index[0].indexTime.frames
-                dur = Double((next - me)) / Double(CSIndexTime.framePerSecond)
-            }else {
-                let me = file.tracks[index].index[lastIndex].indexTime.seconds
-                dur = length - me
-            }
-            
-            var interval = 0.0
-            
-            if file.tracks[index].index.count != 0 {
-                let shortCut = file.tracks[index].index
-                interval = Double(shortCut[shortCut.count - 1].indexTime.frames - shortCut[0].indexTime.frames) / Double(CSIndexTime.framePerSecond)
-            }
-            
-            result.append(CSLengthOfAudio(startTime: calcStartTime, duration: dur))
-            //            file.tracks[index].duration = dur
-            //            file.tracks[index].interval = interval
-            //            file.tracks[index].startTime = CMTime(seconds: calcStartTime, preferredTimescale: 1000)
-            
-            calcStartTime += dur + interval
-        }
-        
-        return result
+        return dCalcTime(lengthOfMusic: length)
     }
     #else
     public func calcTime(lengthOfMusic: Double = 0) -> [CSLengthOfAudio] {
+        return dCalcTime(lengthOfMusic: lengthOfMusic)
+    }
+    #endif
+    
+    private func dCalcTime(lengthOfMusic: Double = 0) -> [CSLengthOfAudio] {
         var calcStartTime = 0.0
         var result = [CSLengthOfAudio]()
         
@@ -115,7 +90,5 @@ public struct CueSheet {
         
         return result
     }
-    #endif
-    
     
 }
