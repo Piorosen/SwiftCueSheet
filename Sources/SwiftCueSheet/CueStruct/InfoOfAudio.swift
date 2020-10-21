07cef59654be3f7b590c726691d82accc2f5b54c
Piorosen <aoikazto@gmail.com>
2020-10-21 14:50:11 +0900

    간단한 파일 이동

--- Sources/SwiftCueSheet/CueStruct/InfoOfAudio.swift
+++ Sources/SwiftCueSheet/CueStruct/InfoOfAudio.swift
@@ -4,28 +4,8 @@
 //
 //  Created by Aoikazto on 2020/06/25.
 //
-#if canImport(AVFoundation)
-import AVFoundation
 
-public struct CSAudio {
-    public init() {
-        lengthOfAudio = CMTime()
-        format = .init()
-        length = .zero
-    }
-    
-    public init(lengthOfAudio: CMTime, format: AVAudioFormat, length: AVAudioFramePosition) {
-        self.lengthOfAudio = lengthOfAudio
-        self.format = format
-        self.length = length
-    }
-    public let lengthOfAudio: CMTime //CMTime
-
-    public let format:AVAudioFormat
-    public let length: AVAudioFramePosition
-}
-#endif
-
+import Foundation
 
 public struct CSLengthOfAudio {
     public init(startTime: Double, endTime: Double) {
