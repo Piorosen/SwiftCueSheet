5bd9651ddea08a48088b1dbc42394fcbc6a38b97
Piorosen <aoikazto@gmail.com>
2020-10-21 21:40:20 +0900

    remove : 데이터 삭제
    유닛 테스트 목록 삭제.
    새롭게 작성할 예정

--- Tests/SwiftCueSheetTests/CueSheetTests.swift
+++ Tests/SwiftCueSheetTests/CueSheetTests.swift
@@ -36,12 +36,7 @@ final class CueSheetTests: XCTestCase {
     }
 
     func testExistCheck2() {
-        if let p = Bundle(for: type(of: self)).path(forResource: "test2", ofType: "cue") {
-            let result = CueSheetParser().load(path: URL(fileURLWithPath: p))
-            XCTAssertNil(result, "파일이 없는데 존재 합니다.")
-        }else {
-            XCTAssert(true, "Error")
-        }
+        
         
     }
 
@@ -50,39 +45,7 @@ final class CueSheetTests: XCTestCase {
 //    470.813 ~ 289.44
 //    764.853 ~ -767.4533333333334
     func testMythRoidTime() {
-//        let tmp = CueSheetParser().load(data: Resources.MYTH_and_ROID_cue)
-//            XCTFail()
-//            return
-//        }
-//        let sheet = CueSheetParser().calcTime(sheet: tmp, lengthOfMusic: 0)
-//
-//        guard let t1 = sheet.file.tracks[0].startTime, let dur1 = sheet.file.tracks[0].duration else {
-//            XCTFail()
-//            return
-//        }
-//        guard let t2 = sheet.file.tracks[1].startTime, let dur2 = sheet.file.tracks[1].duration else {
-//            XCTFail()
-//            return
-//        }
-//        guard let t3 = sheet.file.tracks[2].startTime, let dur3 = sheet.file.tracks[2].duration else {
-//            XCTFail()
-//            return
-//        }
-//        guard let t4 = sheet.file.tracks[3].startTime, let dur4 = sheet.file.tracks[3].duration else {
-//            XCTFail()
-//            return
-//        }
-//        
-//        let epsilon = 0.01
-//        
-//        if  (abs(CMTimeGetSeconds(t1) - 0) < epsilon && abs(dur1 - 291.6533) < epsilon) &&
-//            (abs(CMTimeGetSeconds(t2) - 291.653) < epsilon && abs(dur2 - 176.56) < epsilon) &&
-//            (abs(CMTimeGetSeconds(t3) - 470.813) < epsilon && abs(dur3 - 289.44) < epsilon) &&
-//            (abs(CMTimeGetSeconds(t4) - 764.853) < epsilon && abs(dur4 - (-767.4533)) < epsilon) {
-//            
-//        }else {
-//            XCTFail("유추한 값이 실제론 다릅니다.")
-//        }
+        
     }
 
     static var allTests = [
