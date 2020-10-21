2c771e3da38649abfc64365eec52fba4bb3c9137
Piorosen <aoikazto@gmail.com>
2020-10-21 23:45:31 +0900

    fix : 기본적인 테스트 요소 추가함.

--- Tests/SwiftCueSheetTests/CueSheetTests.swift
+++ Tests/SwiftCueSheetTests/CueSheetTests.swift
@@ -34,23 +34,55 @@ final class CueSheetTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
     }
-
-    func testExistCheck2() {
-        
-        
+    
+    func testMythRoidSaveTime() {
+        let time = CueSheetParser().load(data: CueSheetParser().load(data: Resources.MYTH_and_ROID_cue).save()).calcTime()
+        let ep = 0.001
+        print(time)
+        if abs(time[0].startTime - 0) < ep && abs(time[0].duration - 291.653) < ep &&
+            abs(time[1].startTime - 291.653) < ep && abs(time[1].duration - 176.56) < ep &&
+            abs(time[2].startTime - 470.813) < ep && abs(time[2].duration - 289.44) < ep &&
+            abs(time[3].startTime - 764.853) < ep && abs(time[3].duration - (-767.453)) < ep {
+            
+        }else{
+            XCTFail("잘못된 시간 측정")
+        }
     }
-
-//    0.0 ~ 291.6533333333333
-//    291.653 ~ 176.56
-//    470.813 ~ 289.44
-//    764.853 ~ -767.4533333333334
-    func testMythRoidTime() {
+    
+    func testFaithlessLiveInBerlinTime() {
+        let time = CueSheetParser().load(data: Resources.Faithless_Live_in_Berlin_cue).calcTime(lengthOfMusic: 0)
+        let ep = 0.001
+        if abs(time[0].startTime - 0) < ep && abs(time[0].duration -   402.0) < ep &&
+            abs(time[1].startTime - 402.0 ) < ep && abs(time[1].duration - 252.0) < ep &&
+            abs(time[2].startTime - 654.0 ) < ep && abs(time[2].duration - 370.0) < ep &&
+            abs(time[3].startTime - 1024.0) < ep && abs(time[3].duration - 520.0) < ep &&
+            abs(time[4].startTime - 1544.0) < ep && abs(time[4].duration - 306.0) < ep &&
+            abs(time[5].startTime - 1850.0) < ep && abs(time[5].duration - 454.0) < ep &&
+            abs(time[6].startTime - 2304.0) < ep && abs(time[6].duration - 251.0) < ep &&
+            abs(time[7].startTime - 2555.0) < ep && abs(time[7].duration - -2555.0) < ep {
+            
+        }else {
+            XCTFail("잘못된 시간 측정")
+        }
+    }
         
+    func testMythRoidTime() {
+        let time = CueSheetParser().load(data: Resources.MYTH_and_ROID_cue).calcTime(lengthOfMusic: 0)
+        let ep = 0.001
+        
+        if abs(time[0].startTime - 0) < ep && abs(time[0].duration - 291.653) < ep &&
+            abs(time[1].startTime - 291.653) < ep && abs(time[1].duration - 176.56) < ep &&
+            abs(time[2].startTime - 470.813) < ep && abs(time[2].duration - 289.44) < ep &&
+            abs(time[3].startTime - 764.853) < ep && abs(time[3].duration - (-767.453)) < ep {
+            
+        }else{
+            XCTFail("잘못된 시간 측정")
+        }
     }
 
     static var allTests = [
-        ("testExistCheck2", testExistCheck2),
+        ("testMythRoidSaveTime", testMythRoidSaveTime),
+        ("testFaithlessLiveInBerlinTime", testFaithlessLiveInBerlinTime),
         ("testMythRoidTime", testMythRoidTime)
-        
     ]
 }
