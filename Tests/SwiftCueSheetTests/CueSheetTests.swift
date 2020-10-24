781b1690f0f9c601d7fb3eb26d0f281696cb1360
Piorosen <aoikazto@gmail.com>
2020-10-24 20:43:57 +0900

    feat : 검증용 코드 작성

--- Tests/SwiftCueSheetTests/CueSheetTests.swift
+++ Tests/SwiftCueSheetTests/CueSheetTests.swift
@@ -34,6 +34,35 @@ final class CueSheetTests: XCTestCase {
         super.tearDown()
     }
     
+    func testAdd() {
+        var sheet = CueSheetParser().load(data: Resources.MYTH_and_ROID_cue)
+        _ = sheet.file.tracks.removeTrack(at: 1)
+        
+        let r = sheet.file.tracks.appendTrack(startTime: 298, endTime: (298 + 150), Title: "", trackNum: "")
+        
+        if r {
+            let ep = 0.001
+            
+            let time = sheet.calcTime()
+            for t in time {
+                print("\(t.startTime) : \(t.duration)")
+            }
+            
+            if abs(time[0].startTime - 0) < ep && abs(time[0].duration - 291.653) < ep &&
+                abs(time[1].startTime - 298) < ep && abs(time[1].duration - 150) < ep &&
+                abs(time[2].startTime - 475.413) < ep && abs(time[2].duration - 289.44) < ep &&
+                abs(time[3].startTime - 767.453) < ep && abs(time[3].duration - (-767.453)) < ep {
+                
+            }else {
+                XCTFail()
+            }
+        }else {
+            XCTFail()
+        }
+        
+    }
+    
+    
     func testRemove() {
         var sheet = CueSheetParser().load(data: Resources.MYTH_and_ROID_cue)
 
