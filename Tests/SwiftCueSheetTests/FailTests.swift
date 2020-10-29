67f6852f2b97d7577268dd3b2a0f593712bea15a
Piorosen <aoikazto@gmail.com>
2020-10-29 16:14:45 +0900

    add : 새로운 테스트 케이스 생성

--- Tests/SwiftCueSheetTests/FailTests.swift
+++ Tests/SwiftCueSheetTests/FailTests.swift
@@ -18,18 +18,36 @@ final class FailTests: XCTestCase {
         super.tearDown()
     }
     
-    func testFailTest() {
-        let p = try? CueSheetParser().load(data: Resources.Fail_Cue)
-        print(p ?? "")
+    func testBlankCue() {
+        do {
+            _ = try CueSheetParser().load(data: "1")
+        }catch CSError.blankData {
+            print("pass")
+        }catch {
+            XCTFail("이외의 값이 나오면 안됩니다.")
+        }
     }
     
-    func testFailRem() {
-        let p = try? CueSheetParser().load(data: Resources.Partial_REM_Fail_Cue)
-        print(p ?? "")
+    func testExpireUrl() {
+        let testUrl = URL(fileURLWithPath: "test")
+        do {
+            
+            _ = try CueSheetParser().load(path: testUrl)
+        }catch CSError.expireUrl(let url) {
+            XCTAssertEqual(url, testUrl)
+        }catch {
+            XCTFail("이외의 값이 나오면 안됩니다.")
+        }
     }
+    
+//    func testFailRem() {
+//        let p = try? CueSheetParser().load(data: Resources.Partial_REM_Fail_Cue)
+//        print(p ?? "")
+//    }
     
     static var allTests = [
-        
-        ("testFailTest", testFailTest)
+        ("testBlankCue", testBlankCue),
+        ("testExpireUrl", testExpireUrl)
+//        ("testBlankCue", testBlankCue)
     ]
 }
