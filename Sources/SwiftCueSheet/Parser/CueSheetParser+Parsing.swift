a48a6097ecf7c85e0103a0845bee16cf500b3bd5
Piorosen <aoikazto@gmail.com>
2020-10-30 12:51:47 +0900

    refactor : 코드 수정
    rem, meta, index 관련하여 따로 함수를 돌렸고, 예외 처라 가능하도록 함.
    
    #20 예외를 던지도록 변경함.

--- Sources/SwiftCueSheet/Parser/CueSheetParser+Parsing.swift
+++ Sources/SwiftCueSheet/Parser/CueSheetParser+Parsing.swift
@@ -7,9 +7,53 @@
 
 import Foundation
 
+
 extension CueSheetParser {
     
     // MARK: - Parsing Data
+    
+    fileprivate func makeRem(_ line: String) -> (key:String, value:String)? {
+        let sp = line.split(separator: " ")
+                     .filter { !$0.trimmingCharacters(in: .whitespaces).isEmpty }
+        
+        if sp.count < 3 {
+            return nil
+        }
+        let key = sp[1].uppercased()
+        let value = sp[2...].joined(separator: " ")
+                            .trimmingCharacters(in: ["\"", "\'", " ", "\t", "\n"])
+        
+        return (key, value)
+    }
+    
+    fileprivate func makeMeta(_ line: String) -> (key: String, value:String)? {
+        let sp = line.split(separator: " ").filter { !$0.trimmingCharacters(in: .whitespaces).isEmpty }
+        if sp.count < 2 {
+            return nil
+        }
+        
+        let key = sp[0].uppercased()
+        let value = sp[1...].joined(separator: " ")
+                            .trimmingCharacters(in: ["\"", "\'", " ", "\t", "\n"])
+        
+        return (key, value)
+    }
+    
+    fileprivate func makeIndex(_ line:String) -> CSIndex? {
+        let sp = line.split(separator: " ").filter { !$0.trimmingCharacters(in: .whitespaces).isEmpty }
+        if sp.count < 3 {
+            return nil
+        }
+        guard let num = UInt8(sp[1]) else {
+            return nil
+        }
+        
+        guard let time = CSIndexTime(time: String(sp[2])) else {
+            return nil
+        }
+        
+        return CSIndex(num: num, time: time)
+    }
     
     /**
      File 안에 있는 Rem을 읽고 REM 데이터 로 반환 합니다.
@@ -20,20 +64,14 @@ extension CueSheetParser {
      - Returns:
      CSRem 형식으로 데이터를 반환 합니다. CSRem은 [String:String] 형식입니다.
      */
-    internal func remParser(data:[String]) -> CSRem {
+    internal func remParser(data:[String]) throws -> CSRem {
         var result = CSRem()
-        
+
         for item in data {
-            let sp = item.split(separator: " ")
-            
-            let key = String(sp[1])
-            let value = sp[2...].joined(separator: " ").trimmingCharacters(in: ["\"", "\'", " ", "\t", "\n"])
-            
-            if value.isEmpty {
-                continue
+            guard let rem = makeRem(item) else {
+                throw CSError.rem(text: item, area: data.joined(separator: " "))
             }
-            
-            result[.init(key)] = value
+            result[.init(rem.key)] = rem.value
         }
         
         return result
@@ -48,17 +86,16 @@ extension CueSheetParser {
      - Returns:
      CSMeta 형식으로 데이터를 반환 합니다. CSMeta은 [String:String] 형식입니다.
      */
-    internal func metaParser(data:[String]) -> CSMeta {
+    internal func metaParser(data:[String]) throws -> CSMeta {
         var dicResult = CSMeta()
         
         for item in data {
-            let splited = item.split(separator: " ")
-            let key = String(splited[0].uppercased())
-            let value = String(item[splited[0].endIndex...]).trimmingCharacters(in: ["\"", "\'", " ", "\t", "\n"])
+            guard let meta = makeMeta(item) else {
+                throw CSError.meta(text: item, area: data.joined(separator: "\n"))
+            }
             
-            dicResult[.init(key)] = value
+            dicResult[.init(meta.key)] = meta.value
         }
-        //(dicResult["TITLE"] ?? "", dicResult["PERFORMER"] ?? "", dicResult["SONGWRITER"] ?? "")
         return dicResult
     }
     
@@ -71,47 +108,45 @@ extension CueSheetParser {
      - Returns:
      CSTrack 형식으로 데이터를 반환 합니다.
      */
-    internal func trackParser(data:[String]) -> CSTrack {
+    internal func trackParser(data:[String]) throws -> CSTrack {
         let trackInfo = data[0].split(separator: " ")
-        let fileIndex = Int(trackInfo[1])!
+                                .filter { !$0.trimmingCharacters(in: .whitespaces).isEmpty }
+        if trackInfo.count < 2 {
+            throw CSError.track(text: data[0], area: data.joined(separator: "\n"))
+        }
+        
+        guard let fileIndex = Int(trackInfo[1]) else {
+            throw CSError.track(text: data[0], area: data.joined(separator: "\n"))
+        }
         let fileType = String(trackInfo[2])
         
-        var dicResult = CSMeta()
-        
+        var meta = CSMeta()
         var rem = CSRem()
-        var soundIndex = [CSIndex]()
+        var index = [CSIndex]()
         
-        for index in 1 ..< data.count {
-            let sp = data[index].split(separator: " ")
+        for line in data[1...] {
+            let command = line.split(separator: " ")[0].uppercased()
             
-            let command = String(sp[0]).uppercased()
-            
-            if command == "INDEX" {
-                let indexNum = Int(sp[1])!
-                let indexTime = CSIndexTime(time: String(sp[2]))
-                soundIndex.append(CSIndex(num: UInt8(indexNum), time: indexTime!))
-                
-                continue
-            }else if command == "REM" {
-                let key = String(sp[1])
-                let value = String(sp[2]).trimmingCharacters(in: ["\"", "\'", " ", "\t", "\n"])
-                
-                if value.isEmpty {
-                    continue
+            switch command {
+            case "INDEX":
+                guard let i = makeIndex(line) else {
+                    throw CSError.track(text: line, area: data.joined(separator: "\n"))
                 }
-                
-                rem[.others(key)] = value
-            }else {
-                let value = String(data[index][command.endIndex...]).trimmingCharacters(in: ["\"", "\'", " ", "\t", "\n"])
-                if value.isEmpty {
-                    continue
+                index.append(i)
+            case "REM":
+                guard let r = makeRem(line) else {
+                    throw CSError.track(text: line, area: data.joined(separator: "\n"))
                 }
-                
-                dicResult[.init(command)] = value
+                rem[.init(r.key)] = r.value
+            default:
+                guard let m = makeMeta(line) else {
+                    throw CSError.track(text: line, area: data.joined(separator: "\n"))
+                }
+                meta[.init(m.key)] = m.value
             }
         }
         
-        return CSTrack(trackNum: fileIndex, trackType: fileType, index: soundIndex, rem: rem, meta: dicResult)
+        return CSTrack(trackNum: fileIndex, trackType: fileType, index: index, rem: rem, meta: meta)
     }
     
     /**
@@ -124,22 +159,24 @@ extension CueSheetParser {
      - Returns:
      CSRem 형식으로 데이터를 반환 합니다. CSRem은 [String:String] 형식입니다.
      */
-    internal func fileParser(file:String, track:[[String]]) -> CSFile {
-        let fileInfo = file.split(separator: " ")
+    internal func fileParser(file:String, track:[[String]]) throws -> CSFile {
+        let fileInfo = file.split(separator: " ").filter { !$0.trimmingCharacters(in: .whitespaces).isEmpty }
         
-        if fileInfo.count == 0 {
-            return CSFile(tracks: [CSTrack](), fileName: "", fileType: "")
+        if file.count < 3 {
+            throw CSError.file(text: file)
         }
-        let fileType = String(fileInfo[fileInfo.count - 1])
-        let fileName = String(file.split(separator: "\"")[1])
+        guard let fileType = fileInfo.last else {
+            throw CSError.file(text: file)
+        }
+        let fileName = fileInfo[1..<(fileInfo.endIndex - 1)].joined(separator: " ")
         
         var trackList = [CSTrack]()
         
         for item in track {
-            trackList.append(trackParser(data: item))
+            try trackList.append(trackParser(data: item))
         }
         
-        return CSFile(tracks: trackList, fileName: fileName, fileType: fileType)
+        return CSFile(tracks: trackList, fileName: fileName, fileType: String(fileType))
     }
     
     
