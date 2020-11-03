f0e4febdcf9cb1974fe9c12a7f6f937398fdce35
Piorosen <aoikazto@gmail.com>
2020-11-03 16:26:23 +0900

    latest test

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,13 +2,5 @@ import XCTest
 
 import SwiftCueSheetTests
 
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(ParsingFailTests.allTests),
-        testCase(TimeTests.allTests),
-        testCase(TrackEditTests.allTests),
-        testCase(CueSheetTests.allTests)
-    ]
-}
-//var tests = allTests()
-XCTMain(allTests())
+
+XCTMain(SwiftCueSheetTestsallTests())
--- Tests/SwiftCueSheetTests/XCTestManifests.swift
+++ Tests/SwiftCueSheetTests/XCTestManifests.swift
@@ -1,5 +1,12 @@
 import XCTest
 
-#if !canImport(ObjectiveC)
-
+#if os(Linux)
+public func allTests() -> [XCTestCaseEntry] {
+    return [
+        testCase(SwiftCueSheetTests.allTests),
+        testCase(ParsingFailTests.allTests),
+        testCase(TimeTests.allTests),
+        testCase(TrackEditTests.allTests),
+    ]
+}
 #endif
