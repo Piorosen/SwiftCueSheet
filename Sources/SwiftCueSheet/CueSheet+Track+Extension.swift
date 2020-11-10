c96927caa0fad4a12922e628c9f22d1a07c3a159
Piorosen <aoikazto@gmail.com>
2020-11-10 16:43:17 +0900

    feat : insert 기능 추가함.

--- Sources/SwiftCueSheet/CueSheet+Track+Extension.swift
+++ Sources/SwiftCueSheet/CueSheet+Track+Extension.swift
@@ -17,6 +17,19 @@ extension CueSheet {
         return copy
     }
     
+    public func makeTrack(_ index: Int, time: CSLengthOfAudio, track: CSTrack? = nil) -> CueSheet {
+        var copy = self
+        var t = self.calcTime()
+        t.insert(time, at: index)
+        
+        var tt = self.file.tracks.map { Optional($0) }
+        tt.insert(track, at: index)
+        copy.file.tracks = CSTrackBuilder().setAudioTime(data: t)
+            .setTrackData(data: tt)
+            .build()
+        return copy
+    }
+    
     public func makeTrack(data: [(time: CSLengthOfAudio, track: CSTrack)]) -> CueSheet {
         var copy = self
         copy.file.tracks = CSTrackBuilder().setAudioTime(data: data.map { $0.time })
