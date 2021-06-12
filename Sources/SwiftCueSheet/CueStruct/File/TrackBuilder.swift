fe4b3a283297ffd5e7f62a0c771adfa307de6909
Piorosen <aoikazto@gmail.com>
2021-06-12 23:36:11 +0900

    fix : 데이터 초기화 직관적으로 변경 함.

--- Sources/SwiftCueSheet/CueStruct/File/TrackBuilder.swift
+++ Sources/SwiftCueSheet/CueStruct/File/TrackBuilder.swift
@@ -48,19 +48,24 @@ struct CSTrackBuilder: BuilderProtocol {
         }
         
         if audioTime.count > 0 {
-            var track = CSTrack(trackNum: 0, trackType: "AUDIO", index: [CSIndex](), rem: CSRem(), meta: CSMeta())
+            var track:CSTrack
             if let tmp = tracks.index(of: Int(0)), let t = tmp {
                 track = t
+            }else {
+                track = CSTrack(trackNum: 0, trackType: "AUDIO", index: [CSIndex](), rem: CSRem(), meta: CSMeta())
             }
             
             track.index = [CSIndex(num: 0, time: makeIndex[0])]
             makeTracks.append(track)
             
             for i in 1..<audioTime.count {
-                var track = CSTrack(trackNum: i, trackType: "AUDIO", index: [CSIndex](), rem: CSRem(), meta: CSMeta())
+                // 변수 재사용 (track)
                 if let tmp = tracks.index(of: Int(i)), let t = tmp {
                     track = t
+                }else {
+                    track = CSTrack(trackNum: i, trackType: "AUDIO", index: [CSIndex](), rem: CSRem(), meta: CSMeta())
                 }
+                
                 let trackIndex: [CSIndex]
                 
                 trackIndex = [CSIndex(num: 0, time: makeIndex[i * 2 - 1]), CSIndex(num: 1, time: makeIndex[i * 2])]
